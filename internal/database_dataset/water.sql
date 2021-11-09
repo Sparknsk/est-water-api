d4ae1373293a574317a0c399f46fe3e916ed8ac2
sparknsk <sparknsk@gmail.com>
2021-11-09 14:53:24 +0700

    Dataset + change partition size for test

---
+++ internal/database_dataset/water.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    water (name, model, manufacturer, speed, material, created_at)
+VALUES
+    ('Eclipse', 'Yacht', 'Germany', 100, 'Iron', '2009-01-01T17:50:12.674344Z'),
+    ('Serene', 'Yacht', 'Italy', 200, 'Iron', '2010-01-01T17:51:12.674344Z'),
+    ('Dilbar', 'Yacht', 'Germany', 300, 'Iron', '2015-01-01T17:52:12.674344Z'),
+    ('La Datcha', 'Yacht', 'Netherlands', 400, 'Iron', '2009-01-01T17:53:12.674344Z'),
+    ('Dubai', 'Yacht', 'Germany', 500, 'Iron', '2006-01-01T17:54:12.674344Z'),
+    ('Nsk', 'Boat', 'Russia', 10, 'Wood', '2021-01-01T17:55:12.674344Z'),
+    ('Wake Pro 230', 'Jet ski', 'Russia', 75, 'Iron', '2021-02-01T17:56:12.674344Z'),
+    ('Wake 170', 'Jet ski', 'Russia', 50, 'Iron', '2021-03-01T17:57:12.674344Z');
